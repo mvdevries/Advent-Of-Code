1c371d414595ada3e1a64ce726700481d37d38b2
Martyn de Vries <vriesdemartyn@gmail.com>
2020-11-30 22:15:28 +0100

    Created new files for the first day of code

--- 01a/2020-01.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const AdventOfCodeService = require('../services/advent-of-code');
-
-
-(async () => {
-  const aocService = new AdventOfCodeService();
-  const input = aocService.get();
-  console.log(input);
-})();
