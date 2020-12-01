9ad3b68c99a7d8962a2b809437adf3af7be843b7
Martyn de Vries <vriesdemartyn@gmail.com>
2020-12-01 23:30:17 +0100

    Empty files for day 2

---
+++ 02a/2020-02a.js
@@ -0,0 +1,19 @@
+'use strict';
+const {readFile} = require('fs');
+const {promisify} = require('util');
+const readFileAsync = promisify(readFile);
+
+function readInput() {
+  return readFileAsync('input.txt', 'utf8');
+}
+
+
+
+
+
+
+
+
+(async () => {
+
+})();
