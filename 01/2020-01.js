3757b42d048201da3369e89f5061ab34ecb40305
Martyn de Vries <vriesdemartyn@gmail.com>
2020-12-01 06:26:47 +0100

    01 and 01a done

--- 01/2020-01.js
+++ 01/2020-01.js
@@ -1,5 +1,33 @@
 'use strict';
+const {readFile} = require('fs');
+const {promisify} = require('util');
+const readFileAsync = promisify(readFile);
+
+function readInput() {
+  return readFileAsync('input.txt', 'utf8');
+}
+
+function toEntryArray(input) {
+  return input.split('\n').filter(n => n).map(n => parseInt(n));
+}
+
+function calculate(entries) {
+  const entrySet = new Set(entries)
+
+  for (const entry of entries) {
+    const remainder = 2020 - entry;
+    if (entrySet.has(remainder)) {
+      return entry * remainder;
+    }
+  }
+}
 
 (async () => {
+  const input = await readInput();
+  const entries = toEntryArray(input);
+  console.log(entries)
 
+
+  const answer = calculate(entries);
+  console.log(answer);
 })();
--- 01a/2020-01a.js
+++ 01a/2020-01a.js
@@ -1,5 +1,39 @@
 'use strict';
+const {readFile} = require('fs');
+const {promisify} = require('util');
+const readFileAsync = promisify(readFile);
+
+function readInput() {
+  return readFileAsync('input.txt', 'utf8');
+}
+
+function toEntryArray(input) {
+  return input.split('\n').filter(n => n).map(n => parseInt(n));
+}
+
+function calculate(entries) {
+  const entrySet = new Set(entries)
+
+  for (let i = 0; i < entries.length; i++) {
+    const entry1 = entries[i];
+    const remainder23 = 2020 - entry1;
+
+    for (let j = i + 1; j < entries.length; j++) {
+      const entry2 = entries[j];
+      const remainder3 = remainder23 - entry2;
+
+      if (entrySet.has(remainder3)) {
+        return entry1 * entry2 * remainder3
+      }
+    }
+  }
+}
 
 (async () => {
+  const input = await readInput();
+  let entries = toEntryArray(input);
+  entries.sort();
 
+  const answer = calculate(entries);
+  console.log(answer);
 })();
