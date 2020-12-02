50db950823e13d9c04496b6bf751d361df2edbfb
Martyn de Vries <vriesdemartyn@gmail.com>
2020-12-02 10:30:03 +0100

    Advent solutions of day 2

--- 02/2020-02.js
+++ 02/2020-02.js
@@ -7,15 +7,52 @@ function readInput() {
   return readFileAsync('input.txt', 'utf8');
 }
 
+function toEntryArray(input) {
+  return input.split('\n').filter(n => n);
+}
 
+function parseMinMax(minMax) {
+  const [min, max] = minMax.split('-');
+  return {
+    min,
+    max
+  };
+}
 
+function parseChar(char) {
+  return char.replace(':', '');
+}
 
+function parsePasswordLine(passwordLine) {
+  return passwordLine.split(' ');
+}
 
+function countOccurrences(password, char) {
+  let counter = 0;
+  for (const passwordChar of password) {
+    if (passwordChar === char) {
+      counter++;
+    }
+  }
+  return counter;
+}
 
-
-
+function isValidPassword(occurrences, min, max) {
+  return occurrences >= min && occurrences <= max;
+}
 
 
 (async () => {
+  const validPasswords = toEntryArray(await readInput())
+    .map(pl => {
+      const [minMax, rawChar, password] = parsePasswordLine(pl);
+      const {min, max} = parseMinMax(minMax)
+      const char = parseChar(rawChar)
+      const occurrences = countOccurrences(password, char);
+      return isValidPassword(occurrences, min, max);
+    })
+    .filter(v => v);
+
+  console.log(validPasswords.length);
 
 })();
