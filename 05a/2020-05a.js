4bbaa100d0880d6bf417a26bfbaea5e1ce49bae2
Martyn de Vries <vriesdemartyn@gmail.com>
2020-12-04 08:12:39 +0100

    New files for day 05

---
+++ 05a/2020-05a.js
@@ -0,0 +1,19 @@
+'use strict';
+const {readFile} = require('fs');
+const {promisify} = require('util');
+const readFileAsync = promisify(readFile);
+
+function readInput() {
+  return readFileAsync('input.txt', 'utf8');
+}
+
+function toEntryArray(input) {
+  return input.split('\n');
+}
+
+
+(async () => {
+  const lines = toEntryArray(await readInput());
+
+
+})();
