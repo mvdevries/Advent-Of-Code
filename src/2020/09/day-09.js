67d7db4ea25aafad376a78b85c3d2dc7f503a1ae
Martyn de Vries <vriesdemartyn@gmail.com>
2020-12-09 10:04:40 +0100

    Added day 9 working

--- src/2020/09/day-09.js
+++ src/2020/09/day-09.js
@@ -10,24 +10,79 @@ function readInput() {
 }
 
 function toEntryArray(input) {
-  return input.split('\n').filter(n => n);
+  return input.split('\n').filter(n => n).map(n => parseInt(n));
+}
+
+function calculate(preamble, checkNumber) {
+  const entrySet = new Set()
+
+  for (const entry of preamble) {
+    const remainder = checkNumber - entry;
+    if (entrySet.has(remainder)) {
+      return true;
+    }
+
+    entrySet.add(entry);
+  }
+
+  return false;
+}
+
+function findInvalidNumber(numbers, preambleSize = 25) {
+  for (let i = preambleSize; i < numbers.length; i++) {
+    const num = numbers[i];
+    const preamble = numbers.slice(i - preambleSize, i);
+    if (!calculate(preamble, num)) {
+      return {
+        invalidNumber: num,
+        index: i,
+      };
+    }
+  }
+}
+
+function sum(numbers) {
+  return numbers.reduce((a, n) => a + n, 0);
+}
+
+function findSumCombo(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 2; j < numbers.length; j++) {
+      const subNumbers = numbers.slice(i, j);
+      const s = sum(subNumbers);
+      if (s > target) {
+        break;
+      }
+
+      if (s === target) {
+        return subNumbers;
+      }
+    }
+  }
+}
+
+function min(numbers) {
+  return Math.min.apply(Math, numbers);
+}
+
+function max(numbers) {
+  return Math.max.apply(Math, numbers);
 }
 
 parts.part1 = async function() {
-  const lines = toEntryArray(await readInput());
-
+  const numbers = toEntryArray(await readInput());
+  const {invalidNumber} = findInvalidNumber(numbers);
+  console.log(invalidNumber);
+  return invalidNumber;
 }
 
 parts.part2 = async function() {
-  const lines = toEntryArray(await readInput());
-}
+  const numbers = toEntryArray(await readInput());
+  const {index, invalidNumber} = findInvalidNumber(numbers);
+  const previousNumbers = numbers.slice(0, index).filter(n => n < invalidNumber);
+  const combo = findSumCombo(previousNumbers, invalidNumber);
+  const minMax = max(combo) + min(combo);
+  console.log(minMax);
 
-(async () => {
-  try {
-    await parts.part1();
-    // await parts.part2();
-  } catch (err) {
-    console.log('error');
-    console.log(err);
-  }
-})();
+  return minMax;
+}
--- src/2020/09/day-09.spec.js
+++ src/2020/09/day-09.spec.js
@@ -2,9 +2,9 @@
 const {part1, part2} = require('./day-09');
 
 test('part 1', async () => {
-  expect(await part1()).toBe();
+  expect(await part1()).toBe(32321523);
 });
 
 test('part 2', async () => {
-  expect(await part2()).toBe();
+  expect(await part2()).toBe(4794981);
 });
