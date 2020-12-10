48261665bc4131687a1ac398522799e13e372b1b
Martyn de Vries <vriesdemartyn@gmail.com>
2020-12-10 08:38:51 +0100

    Working day 10

--- src/2020/10/day-10.js
+++ src/2020/10/day-10.js
@@ -10,20 +10,46 @@ function readInput() {
 }
 
 function toEntryArray(input) {
-  return input.split('\n').filter(n => n);
+  return input.split('\n').filter(n => n).map(n => parseInt(n));
+}
+
+function getJoltDiffs(jolts) {
+  return jolts.reduce((acc, jolt) => {
+    const diff = jolt - acc.current;
+    acc[`jolt${diff}`] += 1;
+    acc.current = jolt;
+    return acc;
+  }, { current: 0, jolt1: 0, jolt2: 0, jolt3: 1 });
 }
 
 parts.part1 = async function() {
-  return true;
+  const jolts = toEntryArray(await readInput());
+  jolts.sort((a, b) => a - b);
+  const answers = getJoltDiffs(jolts);
+  return answers.jolt1 * answers.jolt3;
 };
 
+function getAllowedArrangementCount(jolts) {
+  return jolts.reduce((acc, jolt) => {
+    const jolt3 = acc.get(jolt - 3) || 0;
+    const jolt2 = acc.get(jolt - 2) || 0;
+    const jolt1 = acc.get(jolt - 1) || 0;
+    acc.set(jolt, jolt3 + jolt2 + jolt1);
+    return acc;
+  }, new Map([[0, 1]]));
+}
+
 parts.part2 = async function() {
-  return true;
+  const jolts = toEntryArray(await readInput());
+  jolts.sort((a, b) => a - b);
+
+  const countMap = getAllowedArrangementCount(jolts);
+  return [...countMap.values()].pop();
 };
 
 (async () => {
   try {
-    const answer = await parts.part1();
+    const answer = await parts.part2();
     console.log(answer);
   } catch (err) {
     console.log(err);
--- src/2020/10/day-10.spec.js
+++ src/2020/10/day-10.spec.js
@@ -2,9 +2,9 @@
 const {part1, part2} = require('./day-10');
 
 test('part 1', async () => {
-  expect(await part1()).toBe(true);
+  expect(await part1()).toBe(2277);
 });
 
 test('part 2', async () => {
-  expect(await part2()).toBe(false);
+  expect(await part2()).toBe(37024595836928);
 });
