e19d47a6463b6b5f3d36a9e6a8f128976376abc2
Martyn de Vries <vriesdemartyn@gmail.com>
2020-12-12 00:42:44 +0100

    Starting day 12

---
+++ src/2020/12/day-12.js
@@ -0,0 +1,32 @@
+'use strict';
+const {readFile} = require('fs');
+const {promisify} = require('util');
+const readFileAsync = promisify(readFile);
+
+const parts = module.exports = {};
+
+function readInput() {
+  return readFileAsync(__dirname + '/input.txt', 'utf8');
+}
+
+function toEntryArray(input) {
+  return input.split('\n').filter(n => n);
+}
+
+parts.part1 = async function() {
+  let entries = toEntryArray(await readInput());
+
+};
+
+parts.part2 = async function() {
+  let entries = toEntryArray(await readInput());
+
+};
+
+(async () => {
+  try {
+    await parts.part1();
+  } catch(err) {
+    console.log(err);
+  }
+})();
